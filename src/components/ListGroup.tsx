f7dae2b6b2beaaba30cf8c3bb61add7160b19a6b
Daryl Tng <42925677+mrdarylguy@users.noreply.github.com>
2023-07-03 23:41:25 +0800

    Added input to function

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,43 @@
-function ListGroup () {
-    return <h1>List Group</h1>;
+import { useState } from "react";
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  if (items.length == 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item found</p>
+      </>
+    );
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length == 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
